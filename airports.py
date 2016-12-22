f839778c589a79153391e30522607ea757b98c50
Chan Sau Yee <sauyee.c@gmail.com>
2016-12-22 23:51:49 +0100

    Update airports.py, use auth.py

--- airports.py
+++ airports.py
@@ -5,54 +5,25 @@ import json
 import csv
 import traceback
 
+import auth
+
 token_url = 'https://api.lufthansa.com/v1/oauth/token'
-client_id = 'qdjw9vsn48t9brhqhghvj724'
-client_secret = 'db4uNT4NEb'
 csvname = 'airport_dir_v2.csv'
 
-# define data
-data = urllib.parse.urlencode({
-	'client_id': client_id,
-	'client_secret': client_secret,
-	'grant_type':'client_credentials'
-	})
-# encode text data into UTF8
-bdata = data.encode('utf8')
-
-try:
-	rep = urllib.request.urlopen(url=token_url, data=bdata)
-
-	# Convert bytes to string type and string type to dict
-	string = rep.read().decode('utf8')
-	token_json = json.loads(string)
-
-	print("Token is: ", token_json['access_token'])
-
-# if rep.getcode() != 200
-except:
-	print("Token acquisition failed")
-	print(traceback.print_exc())
+def setUrl(offset):
+	url = 'https://api.lufthansa.com/v1/references/airports?lang=EN&limit=100&offset=' + str(offset)
+	return url
 
 try:
 	# create offset variable because max. number of records is 100
 	offset = 0
 	count = 0;
 
+	token = auth.get_token()
+
 	for offset in range(0, 1300, 100):
 
-		# set request url
-		# url2: list directory of airports
-		url2 = 'https://api.lufthansa.com/v1/references/airports?lang=EN&limit=100&offset=' + str(offset)
-
-		# create request with url2
-		req = urllib.request.Request(url2)
-
-		# set header
-		req.add_header('Authorization','Bearer '+ token_json['access_token'])
-		# choose to receive in JSON
-		req.add_header('Accept','application/json')
-
-		rep = urllib.request.urlopen(req).read().decode('utf8')
+		rep = auth.getResponse(setUrl(offset), token)
 		rep_json = json.loads(rep)
 
 		for airport in rep_json['AirportResource']['Airports']['Airport']:
@@ -76,7 +47,6 @@ try:
 				])
 		else:
 			f = csv.writer(open(csvname, "a"))
-
 
 		for airport in rep_json['AirportResource']['Airports']['Airport']:
 			# Check if location is airport (NOT bus and NOT train station)
