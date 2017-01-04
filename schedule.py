c4433b14bbafc3cb16e2151eec58dfd6c1875395
Chan Sau Yee <sauyee.c@gmail.com>
2017-01-04 18:59:50 +0100

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -6,42 +6,70 @@ import traceback
 
 import auth
 
-def setUrl_schedule(month, day):
-	date = '2016-' + str(month) + '-' + str(day)
-	url = 'https://api.lufthansa.com/v1/operations/schedules/FRA/ZRH/' + date
+def setUrl_schedule(origin, destination, month, day):
+	date = '2017-' + month + '-' + day
+	url = 'https://api.lufthansa.com/v1/operations/schedules/' + origin + '/' + destination + '/' + date + '?directFlights=true'
 	return url
 
 try:
 	count = 0
 	token = auth.get_token()
 
-	for day in range(24,32):
-		rep = auth.getResponse(setUrl_schedule(12,day), token)
-		rep_json = json.loads(rep)
+	exportcsv = 'directFlightPairs.csv'
 
-		# export as CSV
-		# if start of export
-		if day == 24:
-			f = csv.writer(open("schedule_dir_v3.csv", "w"))
-			# write CSV headers
-			f.writerow(['count', 'dep1', 'arr1', 'dep2','arr2'])
-		else:
-			f = csv.writer(open("schedule_dir_v3.csv", "a"))
+	# Write csv headers
+	f = csv.writer(open(exportcsv, "w"))
+	f.writerow(['origin', 'des', 'httpcode'])
 
-		for flight in rep_json['ScheduleResource']['Schedule']:
-				count = count + 1
-				if len(flight['Flight']) != 2:
-					f.writerow([count,
-						flight['Flight']['Departure']['AirportCode'],
-						flight['Flight']['Arrival']['AirportCode']])
-				else:
-					f.writerow([count,
-						flight['Flight'][0]['Departure']['AirportCode'],
-						flight['Flight'][0]['Arrival']['AirportCode'],
-						flight['Flight'][1]['Departure']['AirportCode'],
-						flight['Flight'][1]['Arrival']['AirportCode']])
+	with open('Eairports.csv') as csvfile:
+		reader = csv.DictReader(csvfile)
+		lst = list()
+		for row in reader:
+			lst.append(row['airportCode'])
 
-		print('Finished export for day ' + str(day))
+	# Create two lists
+	originList = lst
+	desList = lst
+
+	# Make requests for all possible pairs of European airports
+	for o_item in originList:
+		origin = o_item
+
+		for d_item in desList:
+			destination = d_item
+
+			code = auth.getCode(setUrl_schedule(origin, destination, '01', '20'), token);
+
+			f = csv.writer(open(exportcsv, "a"))
+			f.writerow([o_item, d_item, code])
+
+	# for day in range(24,32):
+	# rep = auth.getResponse(setUrl_schedule(origin, destination, '01', '20'), token);
+	# rep_json = json.loads(rep)
+	#
+	# 	# export as CSV
+	# 	# if start of export
+	# 	if day == 24:
+	# 		f = csv.writer(open("schedule_dir_v3.csv", "w"))
+	# 		# write CSV headers
+	# 		f.writerow(['count', 'dep1', 'arr1', 'dep2','arr2'])
+	# 	else:
+	# 		f = csv.writer(open("schedule_dir_v3.csv", "a"))
+	#
+	# 	for flight in rep_json['ScheduleResource']['Schedule']:
+	# 			count = count + 1
+	# 			if len(flight['Flight']) != 2:
+	# 				f.writerow([count,
+	# 					flight['Flight']['Departure']['AirportCode'],
+	# 					flight['Flight']['Arrival']['AirportCode']])
+	# 			else:
+	# 				f.writerow([count,
+	# 					flight['Flight'][0]['Departure']['AirportCode'],
+	# 					flight['Flight'][0]['Arrival']['AirportCode'],
+	# 					flight['Flight'][1]['Departure']['AirportCode'],
+	# 					flight['Flight'][1]['Arrival']['AirportCode']])
+		#
+		# print('Finished export for day ' + str(day))
 
 except:
 	print("Request failed")
