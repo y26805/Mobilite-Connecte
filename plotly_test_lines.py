6002fda938773073070fd3d8a541054b6e427e7d
Chan Sau Yee <sauyee.c@gmail.com>
2017-01-08 19:06:17 +0100

    Add Plotly EU test plot

---
+++ plotly_test_lines.py
@@ -0,0 +1,61 @@
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import pandas as pd
+
+df_airports = pd.read_csv('airport_clean.csv')
+df_airports.head()
+
+df_flight_paths = pd.read_csv('flights_EU_0120.csv')
+df_flight_paths.head()
+
+airports = [ dict(
+	type = 'scattergeo',
+	lat = df_airports['latitude'],
+	lon = df_airports['longitude'],
+	hoverinfo = 'text',
+	text = df_airports['airport_name'],
+	mode = 'markers',
+	marker = dict(
+		size = 1,
+		opacity = 0.8,
+		line = dict(
+			width = 3,
+			color = 'rgba(68,68,68,0)'
+		)
+	))]
+
+flight_paths = []
+for i in range( len( df_flight_paths ) ):
+    flight_paths.append(
+        dict(
+            type = 'scattergeo',
+            locationmode = 'europe',
+            lon = [ df_flight_paths['start_lon'][i], df_flight_paths['end_lon'][i] ],
+            lat = [ df_flight_paths['start_lat'][i], df_flight_paths['end_lat'][i] ],
+            mode = 'lines',
+            line = dict(
+                width = 1,
+                color = 'red',
+            ),
+            opacity = 0.1,
+        )
+    )
+
+layout = dict(
+	title = '<b>European connections</b><br>(Hover for airport code)<br>Source:<a href="https://developer.lufthansa.com/page">Lufthansa API</a>',
+	geo = dict(
+		projection=dict( type='mercator'),
+		scope = 'europe',
+		showland = True,
+		showcountries = True,
+		landcolor = 'rgb(250,250,250)',
+		subunitcolor = 'rgb(217, 217, 217)',
+		countrycolor = 'rgb(217,217,217)',
+		countrywidth = 2,
+		subunitwidth = 0.5
+		)
+	)
+
+
+plotly.offline.plot({"data":flight_paths + airports, "layout":layout})
