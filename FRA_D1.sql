882c335180d188efa6877fa32100e96506cd4d35
AuroreXi <xiwang4224@gmail.com>
2017-01-31 23:12:20 +0100

    Updating SQL for airport of degree 1 and degree 2

--- FRA_D1.sql
+++ FRA_D1.sql
@@ -1,4 +1,4 @@
-SElECT f.origin, f.des, ad.airportName, ad.lat, ad.lon, cd.cityName, ctrd.name
+SElECT DISTINCT f.origin, f.des, ad.airportName, ad.lat, ad.lon, cd.cityName, ctrd.name
 FROM flights AS f, airports AS ao, airports AS ad, cities AS cd, countries AS ctrd
 WHERE f.origin = ao.airportCode
 AND f.des = ad.airportCode
@@ -7,5 +7,4 @@ AND ad.countryCode = cd.countryCode
 AND cd.countryCode = ctrd.countryCode
 AND f.origin = 'FRA'
 AND f.httpcode = '200'
-AND f.date = '2017-01-20'
 ORDER BY ctrd.name
